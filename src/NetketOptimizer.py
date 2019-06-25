8ee995270ac82f49184224472a37b77760935442
SemyonSinchenko <semyonsinchenko@yahoo.com>
2019-06-25 20:21:12 +0300

    Update NetketOptimizer.py

--- src/NetketOptimizer.py
+++ src/NetketOptimizer.py
@@ -28,16 +28,19 @@ class NetKetOptimizer(object):
                            .CustomHilbert(graph=self.nk_graph, local_states=[-1, 1]))
         sz_sz = [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]
         self.nk_operator = nk.operator.GraphOperator(self.nk_hilbert, bondops=[sz_sz])
-        sys.stdout.write("Cretaed optimizer.")
-        sys.stdout.write("Current state:")
-        for i, v in enumerate(self.nk_sampler.visible):
-            sys.stdout.write("{}th spin orientation is {}".format(i, v))
+
         self.nk_machine = nk.machine.RbmSpin(hilbert=self.nk_hilbert, alpha=1)
         self.nk_machine.init_random_parameters(sigma=0.1)
         self.nk_sampler = (nk
                            .sampler
                            .MetropolisExchange(machine=self.nk_machine,
                                                graph=self.nk_graph))
+
+        sys.stdout.write("Cretaed optimizer.")
+        sys.stdout.write("Current state:")
+        for i, v in enumerate(self.nk_sampler.visible):
+            sys.stdout.write("{}th spin orientation is {}".format(i, v))
+
         self.nk_op = nk.optimizer.Momentum(0.001, 0.9)
         self.nk_fitter = nk.variational.Vmc(
             hamiltonian=self.nk_operator,
